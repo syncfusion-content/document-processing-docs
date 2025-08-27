77f0de1675e4028023f0445ea2a5b878e7d5e7b9
MOHANRAJSF4991 <mohanraj.iyyanar@syncfusion.com>
2025-08-27 13:51:10 +0530

    977732: added parsing code for react ug

---
+++ Document-Processing/code-snippet/Excel/Spreadsheet/React/open-save-cs10/app/app.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { SpreadsheetComponent } from '@syncfusion/ej2-react-spreadsheet';
+
+function App() {
+    const beforeOpen = (args) => {
+        args.parseOptions = {
+            ignoreStyle: true,
+            ignoreFormat: true,
+            ignoreChart: true,
+            ignoreImage: true,
+            ignoreMergedCell: true,
+            ignoreFormula: true,
+            ignoreValidation: true,
+            ignoreConditionalFormat: true,
+        };
+    };
+    return (
+        <SpreadsheetComponent allowOpen={true} openUrl='https://services.syncfusion.com/react/production/api/spreadsheet/open' beforeOpen={beforeOpen} />
+    );
+};
+export default App;
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
