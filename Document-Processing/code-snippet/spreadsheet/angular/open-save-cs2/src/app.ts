c54a74c6a66ad60119eb24a07d9877959b2de346
bhuvaneshdhakshinmaoorthy <bhuvaneshdhakshinmaoorthy>
2026-02-06 14:07:14 +0530

    993171: Added all files

---
+++ Document-Processing/code-snippet/spreadsheet/angular/open-save-cs2/src/app.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { DropDownButtonModule } from '@syncfusion/ej2-angular-splitbuttons'
+import { SpreadsheetAllModule } from '@syncfusion/ej2-angular-spreadsheet'
+
+
+
+import { Component, ViewChild } from '@angular/core';
+import { SpreadsheetComponent } from '@syncfusion/ej2-angular-spreadsheet';
+
+@Component({
+imports: [
+        
+        DropDownButtonModule,
+        SpreadsheetAllModule
+    ],
+
+
+standalone: true,
+    selector: 'app-container',
+    template: "<ejs-spreadsheet #spreadsheet (created)='created()' openUrl='https://document.syncfusion.com/web-services/spreadsheet-editor/api/spreadsheet/open' allowOpen='true'> </ejs-spreadsheet>"
+})
+export class AppComponent {
+    @ViewChild('spreadsheet') public spreadsheetObj?: SpreadsheetComponent;
+     created () {
+        fetch("https://cdn.syncfusion.com/scripts/spreadsheet/Sample.xlsx") // fetch the remote url
+          .then((response) => {
+            response.blob().then((fileBlob) => { // convert the excel file to blob
+            let file = new File([fileBlob], "Sample.xlsx"); //convert the blob into file
+            this.spreadsheetObj!.open({ file: file }); // open the file into Spreadsheet
+            })
+          })
+    }
+}
+
+
