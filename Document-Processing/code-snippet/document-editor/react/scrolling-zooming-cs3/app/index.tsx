324dffc168cd7d218ea8be66089dfc8931c69d64
Sujitha Siva <sujitha.siva@syncfusion.com>
2026-04-10 13:06:59 +0530

    Resolved script error in preview sample

--- Document-Processing/code-snippet/document-editor/react/scrolling-zooming-cs3/app/index.tsx
+++ Document-Processing/code-snippet/document-editor/react/scrolling-zooming-cs3/app/index.tsx
@@ -1,4 +1,3 @@
-
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import {
@@ -62,13 +61,14 @@ DocumentEditorComponent.Inject(
 );
 
 function App() {
-  const documentEditorRef: DocumentEditorComponent = React.useRef(null);
-  const pageCountRef:any = React.useRef(null);
-  const editablePageNumberRef: any = React.useRef(null);
-  const pageNumberLabelRef: any = React.useRef(null);
-  const zoomRef: any = React.useRef(null);
+  const documentEditorRef = React.useRef<DocumentEditorComponent | null>(null);
+  const pageCountRef = React.useRef<HTMLLabelElement | null>(null);
+  const editablePageNumberRef = React.useRef<HTMLDivElement | null>(null);
+  const pageNumberLabelRef = React.useRef<HTMLLabelElement | null>(null);
+  const zoomRef = React.useRef<DropDownButtonComponent | null>(null);
+
   let startPage = 1;
-  let editorPageCount: any;
+  let editorPageCount: number;
 
   const items: ItemModel[] = [
     { text: '200%' },
@@ -88,7 +88,7 @@ function App() {
     const documenteditor = documentEditorRef.current;
 
     if (documenteditor) {
-      documenteditor.viewChange = (e) => updatePageNumberOnViewChange(e);
+      documenteditor.viewChange = (e: any) => updatePageNumberOnViewChange(e);
       documenteditor.contentChange = () => updatePageCount();
 
       if (editablePageNumberRef.current) {
@@ -120,7 +120,7 @@ function App() {
     const editablePageNumber = editablePageNumberRef.current;
     if (
       editablePageNumber?.textContent === '' ||
-      parseInt(editablePageNumber.textContent, 0) > editorPageCount
+      parseInt(editablePageNumber.textContent, 10) > editorPageCount
     ) {
       updatePageNumber();
     }
@@ -130,9 +130,10 @@ function App() {
   function onKeyDown(e: any) {
     const documenteditor = documentEditorRef.current;
     const editablePageNumber = editablePageNumberRef.current;
+
     if (e.which === 13) {
       e.preventDefault();
-      const pageNumber = parseInt(editablePageNumber?.textContent || '0', 0);
+      const pageNumber = parseInt(editablePageNumber?.textContent || '0', 10);
       if (pageNumber > editorPageCount) {
         updatePageNumber();
       } else {
@@ -147,6 +148,7 @@ function App() {
         updatePageNumber();
       }
     }
+
     if (e.which > 64) {
       e.preventDefault();
     }
@@ -163,14 +165,15 @@ function App() {
       documenteditor?.fitPage('FitOnePage');
     } else if (text.match('Fit page width')) {
       documenteditor?.fitPage('FitPageWidth');
-    } else {
-      documenteditor.zoomFactor = parseInt(text, 0) / 100;
+    } else if (documenteditor) {
+      documenteditor.zoomFactor = parseInt(text, 10) / 100;
     }
   }
 
   function updateZoomContent() {
-    if (zoomRef.current) {
-      zoomRef.current.content = Math.round(documentEditorRef.current?.zoomFactor * 100) + '%';
+    if (zoomRef.current && documentEditorRef.current) {
+      zoomRef.current.content =
+        Math.round(documentEditorRef.current.zoomFactor * 100) + '%';
     }
   }
 
@@ -201,35 +204,35 @@ function App() {
     <div>
       <DocumentEditorComponent
         id="container"
-        height={'330px'}
+        height="330px"
         ref={documentEditorRef}
         isReadOnly={false}
-        enablePrint={true}
-        enableSelection={true}
-        enableEditor={true}
-        enableEditorHistory={true}
-        enableContextMenu={true}
-        enableSearch={true}
-        enableOptionsPane={true}
-        enableBookmarkDialog={true}
-        enableBordersAndShadingDialog={true}
-        enableFontDialog={true}
-        enableTableDialog={true}
-        enableParagraphDialog={true}
-        enableHyperlinkDialog={true}
-        enableImageResizer={true}
-        enableListDialog={true}
-        enablePageSetupDialog={true}
-        enableSfdtExport={true}
-        enableStyleDialog={true}
-        enableTableOfContentsDialog={true}
-        enableTableOptionsDialog={true}
-        enableTablePropertiesDialog={true}
-        enableTextExport={true}
-        enableWordExport={true}
+        enablePrint
+        enableSelection
+        enableEditor
+        enableEditorHistory
+        enableContextMenu
+        enableSearch
+        enableOptionsPane
+        enableBookmarkDialog
+        enableBordersAndShadingDialog
+        enableFontDialog
+        enableTableDialog
+        enableParagraphDialog
+        enableHyperlinkDialog
+        enableImageResizer
+        enableListDialog
+        enablePageSetupDialog
+        enableSfdtExport
+        enableStyleDialog
+        enableTableOfContentsDialog
+        enableTableOptionsDialog
+        enableTablePropertiesDialog
+        enableTextExport
+        enableWordExport
       />
       <div id="page-fit-type-div">
-        <label id="page"> Page </label>
+        <label id="page">Page </label>
         <div id="editablePageNumber" ref={editablePageNumberRef}>
           <label id="documenteditor_page_no" ref={pageNumberLabelRef} />
         </div>
@@ -251,5 +254,3 @@ function App() {
 export default App;
 
 ReactDOM.render(<App />, document.getElementById('sample'));
-
-
